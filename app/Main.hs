e687a2611cb223d38b4ef9b05d05a52ea27e1877
Leonid Vlasov <leoniv.vlasov@gmail.com>
2021-04-13 22:36:12 +0800

    Up resolver

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,32 @@
 module Main where
 
-import System.Environment
 import Data.List
+import System.Environment
 import System.Exit
 import System.IO
 
 main :: IO ()
 main = do
   args <- getArgs
-  if null args then undefComand else
-    let (comand:args') = args in dispatch comand args'
+  if null args
+    then undefComand
+    else let (comand : args') = args in dispatch comand args'
 
 usage progname =
   ("Usage: `" ++ progname ++ " command args'") :
-    map ((" " ++ progname ++ " ") ++)
-      [ "add path \"string\""
-       ,"view path num"
-       ,"remove path num"
-      ]
+  map
+    ((" " ++ progname ++ " ") ++)
+    [ "add path \"string\"",
+      "view path num",
+      "remove path num"
+    ]
 
 putsError =
   hPutStrLn stderr
 
 usage' = do
   progName <- getProgName
-  mapM putsError (usage progName)
-  return ()
+  mapM_ putsError (usage progName)
 
 undefComand = do
   usage'
--- src/Data/MonadExplore.hs
+++ src/Data/MonadExplore.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE DeriveFunctor #-}
+
 module Data.MonadExplore where
-import Control.Monad.Writer
+
 import Control.Monad.State
+import Control.Monad.Writer
+import Data.Bifunctor (first, second)
 import Data.Ratio
 
 data Maybe' a = Nothing' | Just' a deriving (Show, Functor)
@@ -20,16 +23,23 @@ main' = do
   b <- Nothing
   return (show a ++ b)
 
-
 type KnightPos = (Int, Int)
 
 moveKnight :: KnightPos -> [KnightPos]
-moveKnight (c, r) = filter onBoard
-   [(c + 2, r - 1), (c + 2, r + 1)
-   ,(c - 2, r - 1), (c - 2, r + 1)
-   ,(c + 1, r - 2), (c + 1, r + 2)
-   ,(c - 1, r - 2), (c - 1, r + 2)]
-     where onBoard (c, r) = c `elem` [1..8] && r `elem` [1..8]
+moveKnight (c, r) =
+  filter
+    onBoard
+    [ (c + 2, r - 1),
+      (c + 2, r + 1),
+      (c - 2, r - 1),
+      (c - 2, r + 1),
+      (c + 1, r - 2),
+      (c + 1, r + 2),
+      (c - 1, r - 2),
+      (c - 1, r + 2)
+    ]
+  where
+    onBoard (c, r) = c `elem` [1 .. 8] && r `elem` [1 .. 8]
 
 logNumber :: Int -> Writer [String] Int
 logNumber x = writer (x, ["Given " ++ show x])
@@ -51,30 +61,30 @@ gcd' a b
     tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
     gcd' b (a `mod` b)
 
-
 addStaf :: Int -> Int
 addStaf = do
-  a <- (*2)
+  a <- (* 2)
   return (a + 10)
 
 type Stack a = [a]
 
 --maybePop :: [Int] -> (Maybe Int, [Int])
 
-
 pop :: Num a => State (Stack a) (Maybe a)
-pop = state maybePop where
-  maybePop [] = (Nothing, [])
-  maybePop (x:xs) = (Just x, xs)
+pop = state maybePop
+  where
+    maybePop [] = (Nothing, [])
+    maybePop (x : xs) = (Just x, xs)
+
 --  \(x:xs) -> (Just x, xs)
 
 push :: Num a => a -> State (Stack a) ()
-push x = state $ \xs -> ((), x:xs)
+push x = state $ \xs -> ((), x : xs)
 
-newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving (Show)
+newtype Prob a = Prob {getProb :: [(a, Rational)]} deriving (Show)
 
 instance Functor Prob where
-  fmap f (Prob xs)= Prob $ map (\(x, p) -> (f x, p)) xs
+  fmap f (Prob xs) = Prob $ map (first f) xs
 
 instance Applicative Prob where
   pure a = Prob [(a, 1)]
@@ -82,29 +92,29 @@ instance Applicative Prob where
 
 instance Monad Prob where
   prob >>= f = flatten (fmap f prob)
-  fail _ = Prob []
 
 flatten :: Prob (Prob a) -> Prob a
-flatten (Prob xs) = Prob $ concatMap multAll $ xs where
-  multAll (Prob innerxs, p) = map (\(a, p') -> (a, p * p')) innerxs
+flatten (Prob xs) = Prob (concatMap multAll xs)
+  where
+    multAll (Prob innerxs, p) = map (second (p *)) innerxs
 
 data Coin = Tails | Heads deriving (Show, Eq)
 
 coin :: Prob Coin
-coin = Prob [(Heads, 1%2), (Tails, 1%2)]
+coin = Prob [(Heads, 1 % 2), (Tails, 1 % 2)]
 
 loadedCoin :: Prob Coin
 loadedCoin = Prob [(Heads, 1 % 10), (Tails, 9 % 10)]
 
 flipThree :: Prob [Coin]
 flipThree = do
-    -- a <- coin
-    b <- coin
-    c <- loadedCoin
-    return [b, c]
+  -- a <- coin
+  b <- coin
+  c <- loadedCoin
+  return [b, c]
 
 foo :: Maybe String
 foo = do
-    x <- Just 3
-    y <- Just "!"
-    Just (show x ++ y)
+  x <- Just 3
+  y <- Just "!"
+  Just (show x ++ y)
